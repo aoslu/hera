aa4ce5c63503e4f109da022a21021ce86985457d
Akın Oslu <aknoslu3541@gmail.com>
2021-07-22 14:48:54 +0300

    deploy onesi

--- hera/settings/base.py
+++ hera/settings/base.py
@@ -19,7 +19,7 @@ env = environ.Env()
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
@@ -31,7 +31,7 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
 
-ALLOWED_HOSTS=['52.59.71.106', '127.0.0.1','52.59.71.106:8000']
+
 
 
 # Application definition
@@ -82,35 +82,9 @@ WSGI_APPLICATION = 'hera.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME': env('DB_NAME'),
-      'USER': env('DB_USER'),
-      'PASSWORD': env('DB_PASSWORD'),
-      'HOST': 'localhost',
-      'PORT': '5432',
-   }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
 # Internationalization
@@ -140,16 +114,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 
-AWS_ACCESS_KEY_ID = env('ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'herakomurculukbucket'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS= {
-    'CacheControl':'max-age=86400',
-}
-AWS_LOCATION = 'static'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-DEFAULT_FILE_STORAGE= 'hera.storage_backend.MediaStorage'
---
+++ hera/settings/production.py
@@ -0,0 +1,44 @@
+from .base import *
+import environ
+
+
+DEBUG=False
+ALLOWED_HOSTS = ['52.59.71.106', '127.0.0.1','52.59.71.106:8000']
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+
+]
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+# DEV ORTAMINA ÖZEL DB.
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME': env('DB_NAME'),
+      'USER': env('DB_USER'),
+      'PASSWORD': env('DB_PASSWORD'),
+      'HOST': 'localhost',
+      'PORT': '5432',
+   }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
