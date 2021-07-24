77349c86ae99a741ceaebb78014411b68b72150c
Akın Oslu <aknoslu3541@gmail.com>
2021-07-24 19:49:46 +0300

    x

--- hera/settings.py
+++ hera/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','52.59.71.106','52.59.71.106:8000','127.0.0.1','localhost:8000']
 
 
 # Application definition
